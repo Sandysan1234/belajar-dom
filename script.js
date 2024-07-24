dc6c3d9ae7e0624812c06c688e7bb12247519c60
Sandysan1234 <rahmahdiandian@gmail.com>
2024-07-24 18:59:16 +0700

    selector

---
+++ script.js
@@ -0,0 +1,47 @@
+// // dom selection
+// // document.getElementById -> megembalikan element
+
+// const pertama = document.getElementById("judul");
+// pertama.style.color= '#FF8225';
+// pertama.style.backgroundColor= '#ccc';
+// pertama.innerHTML = 'Rizky Rahmahdian sandy'
+
+// const section = document.getElementById('b')
+// section.style.backgroundColor = 'blue';     
+
+// // document.getElementsByTagName() -> HTMLCollections
+
+// const pa = document.getElementsByTagName('p');
+
+// for(let i = 0 ; i < pa.length; i++){
+//     pa[i].style.backgroundColor = 'lightblue';
+// }
+
+// const h1=document.getElementsByTagName('h1')[0];
+// h1.style.fontSize = '50px';
+
+
+// // document.getElementsByClassName()
+// // HTMLCollections
+
+// const p1 = document.getElementsByClassName('p1')[0];
+
+
+
+// // document.querySelector() -> element
+// const p4 = document.querySelector("#b p");
+// p4.style.color = "green";
+// p4.style.fontSize = "50px";
+
+// const li2 = document.querySelector("section#b ul li:nth-child(2)");
+// li2.style.backgroundColor = "orange";
+
+// //querySelectorAll()
+// const p = document.querySelectorAll("p");
+// for (let i = 0; i < p.length; i++) {
+//   p[i].style.backgroundColor = "red";
+// }
+// j
+
+
+jjjj
